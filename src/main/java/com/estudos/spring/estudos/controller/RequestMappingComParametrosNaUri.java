4871f874dccd9fda54efe591f3622405607bdbe6
Gustavo A. Ferreira <gustavo.ferreira@bancointer.com.br>
2020-07-09 01:03:32 -0300

    Incluindo parametros opcionais

--- src/main/java/com/estudos/spring/estudos/controller/RequestMappingComParametrosNaUri.java
+++ src/main/java/com/estudos/spring/estudos/controller/RequestMappingComParametrosNaUri.java
@@ -1,5 +1,7 @@
 package com.estudos.spring.estudos.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +31,68 @@ public class RequestMappingComParametrosNaUri {
 	public String getComDoisRequestParamSimples(@RequestParam("id") long id, @RequestParam("id2") long id2) {  
 		return "Usando a anotacao RequestParam, com 2 parâmetros. Primeiro: " + id  + " Segundo: "+ id2; 
 	}
+	//**** PARAMETRO PASSADO PELA URI DE FORMA OPCIONAL
+	//Para que o parametro seja opcional é necessário usar a anotação @RequestParam, sendo necessário
+	//tambem informar com a propriedade required uma vez que por default o Spring mantém o parametro obrigatorio
+	//GetMapping devolvendo uma string
+	@GetMapping("/request_com_param_opcional")
+	public String getComRequestParamOpcionais(@RequestParam(value="id", required=false) String id) {
+		
+		if (id == null) {
+			return "O parametro nao foi passado.";
+		}else {
+			return "Usando a anotacao RequestParam, opcional Parametro " + id;
+		}	
+	}
+
+	//**** MAIS DE UM PARAMETRO PASSADO PELA URI DE FORMA OPCIONAL
+	//Para que o parametro seja opcional é necessário usar a anotação @RequestParam, sendo necessário
+	//tambem informar com a propriedade required uma vez que por default o Spring mantém o parametro obrigatorio
+	//GetMapping devolvendo uma string
+	@GetMapping("/request_com_param_2opcional")
+	public String getComRequestParam2Opcionais(@RequestParam(value="id", required=false) String id, @RequestParam(value="id2", required=false) String id2) {
+		
+		if ((id == null) && (id2 == null)) {
+			return "não passou nenhum parametro";
+		}else 
+		if ((id != null) && (id2 == null)) {
+			return "Trouxe só o primeiro parametro que é " + id;
+		}else
+		if ((id == null) && (id2 != null)) {
+			return  "Trouxe Somente o segundo parametro que é " + id2;
+		}else 
+		if ((id != null) && (id2 != null)) {
+			return "Usando a anotacao RequestParam, opcional Parametro parametro1 " + id + " segundo parametro " + id2;
+		}else
+		return "Nenhuma situacao encontrada.";
+	}
 	
+	//**** MAIS DE UM PARAMETRO PASSADO PELA URI DE FORMA OPCIONAL
+	//Para que o parametro seja opcional é necessário usar a anotação @RequestParam, sendo necessário
+	//tambem informar com a propriedade required uma vez que por default o Spring mantém o parametro obrigatorio
+	//GetMapping devolvendo um ResponseEntity	
+	@GetMapping("/request_com_param_2opcionalComResponseEntity")
+	public ResponseEntity<String> getComRequestParam2OpcionaisComResponseEntity(@RequestParam(value="id", required=false) String id, 
+												@RequestParam(value="id2", required=false) String id2) {
+		String retorno;
+		if ((id == null) && (id2 == null)) {
+			retorno = "não passou nenhum parametro";
+		}else 
+		if ((id != null) && (id2 == null)) {
+			retorno = "Trouxe só o primeiro parametro que é " + id;
+		}else
+		if ((id == null) && (id2 != null)) {
+			retorno =  "Trouxe Somente o segundo parametro que é " + id2;
+		}else 
+		if ((id != null) && (id2 != null)) {
+			retorno = "Usando a anotacao RequestParam, opcional Parametro parametro1 " + id + " segundo parametro " + id2;
+		}else
+			retorno =  "Nenhuma situacao encontrada.";
+		
+		return ResponseEntity.status(HttpStatus.OK).body(retorno);
+	}
+	
+
 	//**** PARAMETRO ENVIADO FAZENDO PARTE DA URI COM @PATHVARIABLE ****
 	//@PathVariable é a anotação que possibilita obter o valor passado como parâmetro na uri da chamada do serviço. Mas não como um parametro que é passado 
 	//apos o sinal de interrogação (?), e sim quando o valor faz parte da uri. Para utilizar, é necessário incluir o path do recurso seguido de barra (/)) 
